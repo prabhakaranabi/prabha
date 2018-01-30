8f19b94bc8c42b1ec6737b13e773964e9a50d509
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-01-30 16:52:58 +0530

    Create prabha4.c

---
+++ prabha4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+char();
+clrscr();
+printf ("enter the character")
+sacnf("%c",&n);
+if(n==a&&n==e==i&&e==o&u)
+{
+printf("alfapht")
+{
+else
+}
+printf ("no");
+{
+getch();
+}
