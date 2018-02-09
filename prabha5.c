0bfd7293c00b8813f4a25a994f099668e31c1c5e
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-09 16:36:18 +0530

    Create prabha5.c

---
+++ prabha5.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b&&a>c)
+{
+printf("%d",a);
+}
+else if(b>a&&b>c)
+{
+printf("%d",b);
+}
+else
+printf("%d",c);
+getch();
+}
+
