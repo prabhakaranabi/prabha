53300acb57130a2ebebaf09d43f0fcb193cc77c1
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-13 16:25:12 +0530

    Rename prabha5.c to 5prabha.c

---
+++ 5prabha.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+inta=6;b=8;c=10
+printf ("greatest three number");
+if (a>b)&&(a>c)
+{
+printf ("a is greater number")
+}
+elseif (b>c)
+{
+printf ("b is greater number")
+}
+else
+{
+printf ("c is greater number")
+{
+getch();
+}
