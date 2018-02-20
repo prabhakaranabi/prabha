6e706a4283e761084ecaa35178a2ae29daffbdc6
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-20 16:40:10 +0530

    Create cdt12.c

---
+++ cdt12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,orginalnum,reverse;
+printf ("enter the number check:");
+scanf("%d",&num);
+whlie(num!=0)
+{
+reverse=reverse*10+num%10;
+num/=10;
+}
+printf ("/n the number is a palidrome");
+{
+else
+{
+printf ("%n its not palidrome");
+}
+return(0);
+}
