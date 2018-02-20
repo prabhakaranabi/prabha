e66185088698a4380802a74d623bfa03c078ff68
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-20 16:08:21 +0530

    Rename prabhaprakaran9.c to cot9.c

---
+++ cot9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inta[]={0,1,2,3,4,5,6,7,8,9};
+int sum=0,n,k,i;
+printf("enter the n value");
+scanf("%d",&n);
+fori=0;i<n;i++)
+printf("%d",a[i];
+printf("enter the k value");
+scanf("%d",&k);
+for(i=0,i<=k;k++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+}
