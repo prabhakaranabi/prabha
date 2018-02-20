50ecad79baf2b025b859de58e441dfa3f39da78a
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-20 16:53:11 +0530

    Create cgd13.c

---
+++ cgd13.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+int num,count=0,1;
+scanf("5d",&&num);
+for(i=1;i<=num:i++)
+{
+if(num%i==0)
+{
+count++;
+}
+}
+if (count==2)
+{
+printf ("prime");
+}
+else
+{
+print ("not prime")
+}
+eturn0;
+}
+
