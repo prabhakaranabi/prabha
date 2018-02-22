5a85e504c0301881fc69da8362a5f77271850557
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-22 16:06:11 +0530

    Create c.prabha16.c

---
+++ c.prabha16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int a,b,flag,i;
+printf ("enter the two limits");
+scanf ("%d%d",&a,&b);
+while (a<b)
+{
+flag=0
+for (i=2,i<a/2;i++)
+{
+if (a%i==0)
+{
+flag=1
+break;
+}
+}
+if (flag==o)
+{
+printf ("%d"&a);
+++a;
+}
+}
