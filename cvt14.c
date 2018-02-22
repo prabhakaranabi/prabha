8bdfd7e59d73ef9afade40a7db4542a9ea3cac15
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-22 15:45:40 +0530

    Create cvt14.c

---
+++ cvt14.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inta,b num;
+printf ("enter the a,b value");
+scanf ("%d%d",&a,&b);
+{
+for i=a,i<=b,i++)
+{
+if(i%2==1)
+print ("print the i value%d",i);
+}
+}
+}
