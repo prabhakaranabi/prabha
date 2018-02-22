5d54186a114236ab3db53d591c14b23ce19fed50
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-02-22 16:34:34 +0530

    Create c.prabha17.c

---
+++ c.prabha17.c
@@ -0,0 +1,20 @@
+#inclde<stdio.h>
+#include<conio.h>
+void main)
+int num ,v,result,digit;
+print ("enter the num value");
+scanf("%d",&num);
+v=num;
+{
+digit=v%10;
+}
+result+=digit*digit*digit;
+v=v%10;
+if(result==num);
+printf ("amglong number");
+{
+else
+}
+printf ("amglong");
+}
+}
