14118832a02083131586aea165c2e20ba3d3dd6c
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-01 15:52:15 +0530

    Create cgt18.c

---
+++ cgt18.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,n1,temp=0,c,1
+printf ("enter the number")
+scanf("%d%d",&n,&m)
+for (i=n,i<m;i++)
+{
+c=n;
+while(n>0)
+{
+n1=n%10;
+temb=temb+(n1*n1*n1);
+n=n/10;
+{
+if (temp==c)
+{
+printf ("armstorng number")
+}
+else
+{
+printf (not armstrong number")
+}
+}
+}
+}
