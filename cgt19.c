a55ffcb2505a3ba22da5f7a75d0d26bc2fff185b
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-01 16:00:55 +0530

    Create cgt19.c

---
+++ cgt19.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+voidd main(0
+{
+intn,fact1,i;
+printf ("enter the value")
+scanf("%d",&n);
+for (i=1,i<n.i++)
+{
+fact=fact*i;
+}
+printf("%d",fact);
+}
+}
+}
