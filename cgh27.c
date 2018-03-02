6476680f1330977bf94c68b10cdc6c16dcd08918
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-02 14:52:20 +0530

    Create cgh27.c

---
+++ cgh27.c
@@ -0,0 +1,29 @@
+#include,stdio.h>
+#include<conio.h>
+void mainchar s[10],i;
+printf("enter a string");
+scanf("%s",&s);
+for(i=0;s[i]!='\0';i++)
+{
+if((s[i]>'a')&&(s[i]<'z'))
+{
+printf("no numeric");
+
+}
+else
+{
+    printf("%c\n",s[i]);
+}
+}}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+About
