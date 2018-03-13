d75f32509ed9ba86bc5f1bcba9e9f8dc1f1efb27
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-13 16:51:04 +0530

    Create prabha26.c

---
+++ prabha26.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#inclde<conio.h>
+voidchar str[10],i,c=0;
+printf("enter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i]>='0')&&(str[i]<='9'))
+{
+c++;
+}
+}
+printf("%d",c);
+ main()
+}
+}
+}
+}
