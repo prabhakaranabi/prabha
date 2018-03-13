1e4d60ec6d06c2eec4d8e85ebc74cab17ef3c193
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-13 15:59:41 +0530

    Create prabha41.c

---
+++ prabha41.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+char [har str[50],i,c=0;
+printf("enter a character");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]>'a' &&str[i]<'z')
+{
+c++;
+}
+}
+printf("%d",c);
+char;
+}
+}
+}
