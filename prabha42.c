291e78b369a2ff917a6aa1fb52846db601cf945a
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-13 16:12:33 +0530

    Create prabha42.c

---
+++ prabha42.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+main vo{
+char str[100,i,c=0;
+printf("enter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+if(str[i]==' ')
+{
+c++;
+}
+}
+printf("%d",c+1);
+}id()
