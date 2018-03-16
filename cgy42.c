e8980e3d9ee7ea5012ce11ac56a8fa08c45b7d61
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-16 14:06:11 +0530

    Create cgy42.c

---
+++ cgy42.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>char str[5];
+  int k,i;
+printf("enter a string");
+scanf("%s",&str);
+printf("enter a limit");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("%s",str);
+}
+
+void main()
