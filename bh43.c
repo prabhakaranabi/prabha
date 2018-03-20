4d39e543ca03c01e0af76cd1487b7c1e9cbe55bf
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-20 15:53:29 +0530

    Create bh43.c

---
+++ bh43.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+voidvoid main()
+{
+char str[30],str1[40];
+int i,j;
+printf("enter 2 strings");
+gets(str);
+gets(str1);
+while(str[i]!='\0')
+{
+    i++;
+    
+}
+while(str1[j]!='\0')
+{
+    str[i]=str1[j];
+    i++;
+    j++;
+
+}
+ main()
+}
