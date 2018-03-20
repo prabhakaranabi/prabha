42281222c87473397034c4da72f55939907e43d2
prabhakaranabi <35652757+prabhakaranabi@users.noreply.github.com>
2018-03-20 16:01:01 +0530

    Create bh44.c

---
+++ bh44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+vvoid main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n>1)&&(n<10))
+{
+printf("%d",n);
+}
+else
+{
+    printf("not in range");
+}
+}oid main()
